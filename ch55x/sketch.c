889c7e10cf94ed328b6166a24b887bba4c54997a
Yury Botov <yury.botov@gmail.com>
2020-04-29 13:57:43 +0300

    adc ok

--- ch55x/sketch.c
+++ ch55x/sketch.c
@@ -4,6 +4,7 @@ void setup() {
     Serial0Begin(19200);
 
     pinMode(PWM15,OUTPUT);
+    pinMode(A11,INPUT);
 }
 
 byte state = 0;
@@ -11,10 +12,11 @@ dword moment = 0;
 int i;
 
 void loop() {
-    if(Serial0Available() > 0) Serial0Putc(Serial0Read());
+    //if(Serial0Available() > 0) Serial0Putc(Serial0Read());
 
     if(moment < millis()) {
-        moment = millis() + 100;
+        moment = millis() + 1000;
+        Serial0Printf("Adc11 = %d\n\r",analogRead(A11));
         analogWrite(PWM15,state);
         state += 20;
         if(state>250) state = 0;
